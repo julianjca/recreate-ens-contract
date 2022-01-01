06d398fe468847998d2b578561df8132605e8426
Julian Anderson <juliancanderson@gmail.com>
2022-01-01 09:32:39 +0700

    feat: adding prettier and solhint

--- src/ENS.sol
+++ src/ENS.sol
@@ -8,31 +8,32 @@ import "./Ownable.sol";
 error DoesNotExist();
 
 contract ENS is ERC721, Ownable {
-  uint256 public constant TOTAL_SUPPLY = 10_000;
-  uint256 public constant PRICE_PER_MINT = 0.05 ether;
+    uint256 public constant TOTAL_SUPPLY = 10_000;
+    uint256 public constant PRICE_PER_MINT = 0.05 ether;
 
-  string public baseURI;
-  constructor(
-      string memory _name,
-      string memory _symbol,
-      string memory _baseURI
-  ) payable ERC721(_name, _symbol) {
-      baseURI = _baseURI;
-  }
+    string public baseURI;
 
-  function tokenURI(uint256 id) public view override returns (string memory) {
-      if (ownerOf[id] == address(0)) revert DoesNotExist();
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseURI
+    ) payable ERC721(_name, _symbol) {
+        baseURI = _baseURI;
+    }
 
-      return string(abi.encodePacked(baseURI, id));
-  }
+    function tokenURI(uint256 id) public view override returns (string memory) {
+        if (ownerOf[id] == address(0)) revert DoesNotExist();
 
-  function withdraw() external {
-      // if (msg.sender != _owner) revert NotOwner();
+        return string(abi.encodePacked(baseURI, id));
+    }
 
-      SafeTransferLib.safeTransferETH(msg.sender, address(this).balance);
-  }
-  
-  function supportsInterface(bytes4 interfaceId)
+    function withdraw() external {
+        // if (msg.sender != _owner) revert NotOwner();
+
+        SafeTransferLib.safeTransferETH(msg.sender, address(this).balance);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
         public
         pure
         override(ERC721, Ownable)
@@ -44,5 +45,4 @@ contract ENS is ERC721, Ownable {
             interfaceId == 0x5b5e139f || // ERC165 Interface ID for ERC165
             interfaceId == 0x01ffc9a7; // ERC165 Interface ID for ERC721Metadata
     }
-
 }
