2c087d3922b21901a09f3173dfdee3bda176a785
Julian Anderson <juliancanderson@gmail.com>
2022-01-01 09:11:32 +0700

    feat: adding transferOwnership function

--- src/test/Ownable.t.sol
+++ src/test/Ownable.t.sol
@@ -14,4 +14,10 @@ contract OwnableTest is DSTest {
     function testOwnerIsMsgSender() public {
         assertEq(token.onwer(), address(this));
     }
+
+    function testTransferOwnership() public {
+        address newOwner = 0xeb2d7106A5728ACCBdBe380C152e2307a0Cc8FAf;
+        token.transferOwnership(newOwner);
+        assertEq(token.onwer(), newOwner);
+    }
 }
